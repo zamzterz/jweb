b0c1c85538e394a64d59699782890dd1088ff17d
Samuel Gulliksson <samuel.gulliksson@gmail.com>
2017-10-29 21:52:13 +0100

    Add support for decrypting JWE with RSA private key from file.

--- src/Main.hs
+++ src/Main.hs
@@ -10,9 +10,9 @@ import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as C
 import qualified Crypto.PubKey.RSA as RSA
 
+import OpenSSL.EVP.PKey
 import OpenSSL.PEM
 import OpenSSL.RSA
-import OpenSSL.EVP.PKey
 
 import System.Console.CmdArgs
 import System.Exit (exitFailure)
@@ -53,6 +53,49 @@ handleEncryption payload publicKeyPath = do
         Right (Jwt jwt) -> print jwt
         Left error -> print error
 
+handleDecryption jwt privateKeyPath = do
+    decrypted <- decrypt privateKeyPath (C.pack jwt)
+    case decrypted of
+        Right (Jwe (hdr, claims)) -> putStrLn (show hdr ++ "." ++ show claims)
+        Left error -> print error
+
+decrypt :: FilePath -> B.ByteString -> IO (Either JwtError JwtContent)
+decrypt privateKeyPath jwt = do
+    privateKey <- createPrivateKeyJwk privateKeyPath
+    unpackJwe privateKey jwt
+
+createPrivateKeyJwk :: FilePath -> IO Jwk
+createPrivateKeyJwk rsaKeyFilePath = do
+    rsaPrivateKeyData <- readFile rsaKeyFilePath
+    parsedRsaPrivateKey <- readPrivateKey rsaPrivateKeyData PwNone
+    let rsaPrivateKey = toKeyPair parsedRsaPrivateKey :: Maybe RSAKeyPair
+    case rsaPrivateKey of
+        Just (rsa) -> return (privateRsaKeyToJwk rsa)
+        Nothing -> do
+            putStrLn "Could not convert RSA key to private key"
+            exitFailure
+
+privateRsaKeyToJwk :: RSAKeyPair -> Jwk
+privateRsaKeyToJwk rsa = RsaPrivateJwk (RSA.PrivateKey
+                         { RSA.private_pub = rsaPub
+                         , RSA.private_d = d
+                         , RSA.private_p = p
+                         , RSA.private_q = q
+                         , RSA.private_dP = maybe 0 id dp
+                         , RSA.private_dQ = maybe 0 id dq
+                         , RSA.private_qinv = maybe 0 id qinv
+                         }) Nothing Nothing Nothing
+                        where rsaPub = RSA.PublicKey { RSA.public_size = rsaSize rsa, RSA.public_n = rsaN rsa, RSA.public_e = rsaE rsa }
+                              d = rsaD rsa
+                              p = rsaP rsa
+                              q = rsaQ rsa
+                              dp = rsaDMP1 rsa
+                              dq = rsaDMQ1 rsa
+                              qinv = rsaIQMP rsa
+
+unpackJwe :: Jwk -> B.ByteString -> IO (Either JwtError JwtContent)
+unpackJwe privateKey jwt = jwkDecode privateKey jwt
+
 data Jweb = Encrypt {payload :: String, publicKeyPath :: FilePath}
           | Decrypt {jwt :: String, privateKeyPath :: FilePath}
             deriving (Data, Typeable, Show, Eq)
@@ -71,4 +114,4 @@ main = do
     command <- cmdArgs options
     case command of
         Encrypt payload publicKeyPath -> handleEncryption payload publicKeyPath
-        Decrypt jwt privateKeyPath -> print "Decryption is not yet supported!"
+        Decrypt jwt privateKeyPath -> handleDecryption jwt privateKeyPath
