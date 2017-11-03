48f3de1302afc7ba369fa50c9e397788c7b04563
Samuel Gulliksson <samuel.gulliksson@gmail.com>
2017-11-03 21:08:29 +0100

    Fix #3: Move encryption and decryption functions to separate modules.
    Also remove use of exitFailue; propagate key parsing errors instead.

---
+++ src/Decrypt.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Decrypt (decrypt) where
+
+import Jose.Jwa
+import Jose.Jwe
+import Jose.Jwk
+import Jose.Jwt
+
+import qualified Data.ByteString as B
+
+import qualified Crypto.PubKey.RSA as RSA
+import OpenSSL.EVP.PKey
+import OpenSSL.PEM
+import OpenSSL.RSA
+
+import Crypto.Random (MonadRandom)
+
+-- | Decrypt a JWE with the private key given by the file path.
+decrypt :: FilePath -> B.ByteString -> IO (Either JwtError JwtContent)
+decrypt privateKeyPath jwt = do
+    privateKey <- createPrivateKeyJwk privateKeyPath
+    case privateKey of
+        Just key -> unpackJwe key jwt
+        Nothing -> return (Left (KeyError "Could not parse private key"))
+
+-- | Create a JWK from a private RSA key in PEM format read from file.
+createPrivateKeyJwk :: FilePath -> IO (Maybe Jwk)
+createPrivateKeyJwk rsaKeyFilePath = do
+    rsaPrivateKeyData <- readFile rsaKeyFilePath
+    parsedRsaPrivateKey <- readPrivateKey rsaPrivateKeyData PwNone
+    let rsaPrivateKey = toKeyPair parsedRsaPrivateKey :: Maybe RSAKeyPair
+    return (fmap privateRsaKeyToJwk rsaPrivateKey)
+
+-- Map a private key in a RSA key pair to a JWK.
+privateRsaKeyToJwk :: RSAKeyPair -> Jwk
+privateRsaKeyToJwk rsa = RsaPrivateJwk (RSA.PrivateKey
+                         { RSA.private_pub = rsaPub
+                         , RSA.private_d = d
+                         , RSA.private_p = p
+                         , RSA.private_q = q
+                         , RSA.private_dP = maybe 0 id dp
+                         , RSA.private_dQ = maybe 0 id dq
+                         , RSA.private_qinv = maybe 0 id qinv
+                         }) Nothing Nothing Nothing
+                        where rsaPub = RSA.PublicKey { RSA.public_size = rsaSize rsa, RSA.public_n = rsaN rsa, RSA.public_e = rsaE rsa }
+                              d = rsaD rsa
+                              p = rsaP rsa
+                              q = rsaQ rsa
+                              dp = rsaDMP1 rsa
+                              dq = rsaDMQ1 rsa
+                              qinv = rsaIQMP rsa
+
+-- | Decrypt data with the given key.
+unpackJwe :: MonadRandom m => Jwk -> B.ByteString -> m (Either JwtError JwtContent)
+unpackJwe privateKey jwt = jwkDecode privateKey jwt
+
---
+++ src/Encrypt.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Encrypt (encrypt) where
+
+import Jose.Jwa
+import Jose.Jwe
+import Jose.Jwk
+import Jose.Jwt
+
+import qualified Data.ByteString as B
+
+import qualified Crypto.PubKey.RSA as RSA
+import OpenSSL.EVP.PKey
+import OpenSSL.PEM
+import OpenSSL.RSA
+
+import Crypto.Random (MonadRandom)
+
+-- | Create a JWE, encrypting the given data with the public key given by the file path.
+encrypt :: FilePath -> B.ByteString -> IO (Either JwtError Jwt)
+encrypt publicKeyPath claims = do
+    publicKey <- createPublicKeyJwk publicKeyPath
+    case publicKey of
+        Just key -> createJwe key claims
+        Nothing -> return (Left (KeyError "Could not parse public key"))
+
+-- | Create a JWK from a public RSA key in PEM format read from file.
+createPublicKeyJwk :: FilePath -> IO (Maybe Jwk)
+createPublicKeyJwk rsaKeyFilePath = do
+    rsaPublicKeyData <- readFile rsaKeyFilePath
+    parsedRsaPublicKey <- readPublicKey rsaPublicKeyData
+    let rsaPublicKey = toPublicKey parsedRsaPublicKey :: Maybe RSAPubKey
+    return (fmap publicRsaKeyToJwk rsaPublicKey)
+
+-- Map a public RSA key to a JWK.
+publicRsaKeyToJwk :: RSAPubKey -> Jwk
+publicRsaKeyToJwk rsa = RsaPublicJwk (RSA.PublicKey
+                         { RSA.public_size = size
+                         , RSA.public_n = n
+                         , RSA.public_e = e
+                         }) Nothing Nothing Nothing
+                        where size = rsaSize rsa
+                              n = rsaN rsa
+                              e = rsaE rsa
+
+-- | Encrypt data with the given key.
+createJwe :: MonadRandom m => Jwk -> B.ByteString -> m (Either JwtError Jwt)
+createJwe publicKey claims = jwkEncode RSA_OAEP A128GCM publicKey (Claims claims)
