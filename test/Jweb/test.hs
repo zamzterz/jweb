b0ec99fe7e9c39bf8a0051095da0e5f468b030ff
Samuel Gulliksson <samuel.gulliksson@gmail.com>
2017-11-07 22:21:30 +0100

    Fix #5: Add application with simple REST API for encryption and decryption.

--- test/Jweb/test.hs
+++ test/Jweb/test.hs
@@ -1,20 +1,31 @@
+{-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
+
+import Test.Hspec
+import Test.Hspec.Wai
+import Test.Hspec.Wai.JSON
 import Test.Tasty
+import Test.Tasty.Hspec
 import Test.Tasty.HUnit
 import Test.Tasty.Program
 
 import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString.Lazy.Char8 as L
 
 import Jose.Jwk
 import Jose.Jwt
 import Jweb.Encrypt
 import Jweb.Decrypt
 
-main = defaultMain tests
+import Jweb.App (waiApp)
 
-tests :: TestTree
-tests = testGroup "Tests" [jwebUnitTests, cliTests]
+import Network.HTTP.Types.URI (urlEncode)
+
+main = do
+    apiTests <- restTests
+    defaultMain (testGroup "Tests" [jwebUnitTests, cliTests, apiTests])
 
 testJwe = "eyJhbGciOiJSU0EtT0FFUCIsImVuYyI6IkExMjhHQ00ifQ.Gl_JYiuaDbTiASvXoxdAU2srn9bolXBEd7h5eagCbwx1hxBWi-SLkKf34sgg0DIBwJ7DxNSDgtOrFcNxLWcy1w.3mfMQb0YVn_cmCdv.e2G1sA.sQkBo1l32d3Xunw9VNPK9A"
+testData = "test"
 
 jwebUnitTests = testGroup "Jweb lib: unit tests"
     [ testCaseSteps "Should be able to encrypt and decrypt data with RSA key" $ \step -> do
@@ -62,3 +73,45 @@ cliTests = testGroup "Jweb CLI: application test"
     [ testProgram "Should successfully encrypt" "jweb-cli" ["encrypt", "data", "test/key1.pub"] Nothing
     , testProgram "Should successfully decrypt" "jweb-cli" ["decrypt", testJwe, "test/key1.priv"] Nothing
     ]
+
+specTests = with waiApp $ do
+    let matchPartial expected = MatchBody $ \_ body -> case C.isInfixOf expected (L.toStrict body) of
+         True  -> Nothing
+         False -> Just $  "Expected to find somewhere in body: " ++ C.unpack expected ++ ",\n"
+                    ++ "Found: " ++ L.unpack body
+    describe "/api/encrypt" $ do
+        it "encrypts payload with a public key" $ do
+            publicKeyData <- liftIO $ readFile "test/key1.pub"
+            postHtmlForm "/api/encrypt" [("key", publicKeyData), ("payload", testData)] `shouldRespondWith` 200 {matchBody = matchPartial "\"jwe\":"}
+
+        it "gives an error when using a private key" $ do
+            privateKeyData <- liftIO $ readFile "test/key1.priv"
+            postHtmlForm "/api/encrypt" [("key", privateKeyData), ("payload", testData)] `shouldRespondWith` [json| {error: "Could not parse public key"} |] {matchStatus = 400}
+
+        it "gives an error for request missing 'payload' parameter" $ do
+            privateKeyData <- liftIO $ readFile "test/key1.priv"
+            postHtmlForm "/api/encrypt" [("key", privateKeyData)] `shouldRespondWith` [json| {error: "Param: payload not found!"} |] {matchStatus = 400}
+
+        it "gives an error for request missing 'key' parameter" $ do
+            privateKeyData <- liftIO $ readFile "test/key1.priv"
+            postHtmlForm "/api/encrypt" [("payload", testData)] `shouldRespondWith` [json| {error: "Param: key not found!"} |] {matchStatus = 400}
+
+    describe "/api/decrypt" $ do
+        it "decrypts payload with a public key" $ do
+            privateKeyData <- liftIO $ readFile "test/key1.priv"
+            postHtmlForm "/api/decrypt" [("key", privateKeyData), ("jwe", testJwe)] `shouldRespondWith` [json| {header: {alg:"RSA-OAEP", enc: "A128GCM"}, payload: #{testData} } |]
+
+        it "gives an error when using a public key" $ do
+            publicKeyData <- liftIO $ readFile "test/key1.pub"
+            postHtmlForm "/api/decrypt" [("key", publicKeyData), ("jwe", testJwe)] `shouldRespondWith` [json| {error: "Could not parse private key"} |] {matchStatus = 400}
+
+        it "gives an error for request missing 'jwe' parameter" $ do
+            privateKeyData <- liftIO $ readFile "test/key1.priv"
+            postHtmlForm "/api/decrypt" [("key", privateKeyData)] `shouldRespondWith` [json| {error: "Param: jwe not found!"} |] {matchStatus = 400}
+
+        it "gives an error for request missing 'key' parameter" $ do
+            privateKeyData <- liftIO $ readFile "test/key1.priv"
+            postHtmlForm "/api/decrypt" [("jwe", testJwe)] `shouldRespondWith` [json| {error: "Param: key not found!"} |] {matchStatus = 400}
+
+restTests :: IO TestTree
+restTests = testSpec "Jweb REST API: application test" specTests
